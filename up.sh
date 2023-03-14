0c786139e9db2ff644085750a1ebc36c0eb6388d
b17Owner <aftar.r0ot@ya.ru>
2023-03-14 16:34:54 +0600

    rm up.sh file

--- up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose build && \
-docker-compose run checker python check_blocked.py -f dmns.list && \
-docker-compose run checker python check_blocked.py -f url.list && \
-docker-compose run checker python check_blocked.py -f ip.list
